98012df4a6eb707f8e4cb0b8ded8f3664d1fe460
venkatthiru1122 <venkadesht285@gmail.com>
2022-10-14 20:38:04 +0530

    super santhosh

---
+++ src/org/ele/ElectionAge.java
@@ -0,0 +1,23 @@
+package org.ele;
+
+public class ElectionAge {
+	
+	public static void main (String[] args) {
+		int a = 10;
+		
+		if (a>=18) {
+			System.out.println("Eligible");	
+
+			
+		} else {
+			System.out.println("Not Eligible");	
+
+
+		}
+		
+
+	}
+	
+	
+
+}
